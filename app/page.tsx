771cd56ed8b7456a2c392dfc6031c0ee6a53114a
Sven Fydrich <sven.fydrich@hm.edu>
2025-06-16 15:33:16 +0200

    Continue implementing

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const lexend = Lexend({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className={`min-h-screen p-4 md:p-8 bg-gray-50 ${lexend.className}`}>
+    <div className={`min-h-screen p-4 md:p-8 bg-white ${lexend.className}`}>
       {/* Navbar */}
       <nav className="max-w-7xl mx-auto">
         <h1 className="text-black text-center text-2xl md:text-[30px]">
@@ -49,7 +49,7 @@ export default function Home() {
             </ul>
             <div className="relative w-full aspect-[4/3] hidden md:block">
               <Image
-                src="/carousel/group_image.png"
+                src="/misc/group_image.png"
                 alt="Project Team"
                 fill
                 className="object-cover"
@@ -65,7 +65,7 @@ export default function Home() {
             <div className="flex flex-col md:flex-row items-start gap-30">
               {/* Text section */}
               <div className="flex flex-col">
-                <h1 className="text-black text-right text-2xl md:text-[30px] font-bold">
+                <h1 className="text-black text-right text-2xl pl-5f md:text-[30px] font-bold">
                   Ausstellung
                 </h1>
                 <span className="text-black text-right text-xs mt-2">
@@ -76,12 +76,26 @@ export default function Home() {
               {/* Image section */}
               <div className="relative w-full md:w-[383px] h-[500px] hidden md:block">
                 <Image
-                  src="/carousel/austellung.png"
+                  src="/misc/austellung.png"
                   alt="Flora.exe Ausstellung"
                   fill
                   className="object-cover object-right"
                 />
               </div>
+            </div>
+            <div>
+              <h1 className="text-black pt-30 pl-5 pb-5 font-bold text-[30px]">
+                Konzept
+              </h1>
+              <span className="text-black pl-5">
+                Was passiert, wenn wir der Natur endlich zuhören?
+              </span>
+              <p className="text-black pt-5 pl-5 w-[70%]">
+                Wir haben Bildschirme gebaut, die um unsere Aufmerksamkeit
+                kämpfen. Aber was, wenn die Natur dasselbe tut? flora.exe lenkt
+                unseren Blick zurück – auf das, wovon wir abhängen. Lorem ipsum
+                blabla
+              </p>
             </div>
           </div>
         </div>
@@ -95,6 +109,125 @@ export default function Home() {
         <span className="text-black text-center text-xs">
           Wie reagiert die Pflanze?
         </span>
+      </div>
+
+      {/* Banner section */}
+      <div>
+        <div className="flex flex-col items-center p-10">
+          <Image
+            src="/banners/nähe_banner.png"
+            alt="Nähe"
+            width={1168}
+            height={259}
+            className=""
+          />
+        </div>
+        <div className="flex flex-col items-center p-10">
+          <Image
+            src="/banners/ablenkung_banner.png"
+            alt="Nähe"
+            width={1168}
+            height={259}
+            className=""
+          />
+        </div>
+        <div className="flex flex-col items-center p-10">
+          <Image
+            src="/banners/stille_banner.png"
+            alt="Nähe"
+            width={1168}
+            height={259}
+            className=""
+          />
+        </div>
+      </div>
+
+      {/* Totaler Zerfall section */}
+      <div className="mx-auto max-w-7xl mt-20 w-[1168px]">
+        <div className="mx-auto w-full max-w-[1168px] h-[3px] bg-black" />
+        <div className="flex">
+          <Image
+            src="/misc/totaler_zerfall.png"
+            alt="Totaler Zerfall"
+            width={459}
+            height={594}
+            className=""
+          />
+          <div className="flex flex-col pt-10 pl-20 w-[40%]">
+            <h1 className="text-black font-bold text-[30px]">Emotionen</h1>
+            <span className="text-black pt-5">
+              Skalen der Gefühle der Pflanze
+            </span>
+            <h2 className="text-black font-bold text-[18px] pt-5">
+              Benutzter prompt
+            </h2>
+            <span className="text-black pt-5 text-justify text-xs">
+              expressionist oil painting in the style of Edvard Munch's 'The
+              Scream', figure collapsing inward, mouth open in silent scream,
+              colors shattered into shards of deep crimson and grey, sky
+              disintegrated, bridge broken in places, figure semi-transparent as
+              if fading, canvas cracked and burned, atmosphere of extreme inner
+              collapse and existential panic, as if the natural world itself is
+              crying out after being utterly ignored and consumed by digital
+              distraction.
+            </span>
+            <h2 className="text-black font-bold text-[18px] pt-5">
+              Wann aktiv?
+            </h2>
+            <span className="text-black pt-5 text-justify text-xs">
+              Lorem ipsum dolor sit amet consectetur. Enim elementum nulla nunc
+              posuere fermentum a id. Egestas nibh nam elit elit commodo luctus
+              ipsum. Ultricies a diam sapien eu eget feugiat. Velit consequat
+              orci aliquam morbi sed non sed.
+            </span>
+          </div>
+        </div>
+        <div className="mx-auto w-full max-w-[1168px] h-[3px] bg-black" />
+      </div>
+
+      {/* Trailer section */}
+      <div className="mt-10 flex flex-col items-center pt-20">
+        <h1 className="text-black text-center text-2xl md:text-[30px] font-bold">
+          Trailer
+        </h1>
+        <span className="text-black text-center text-xs">
+          Behind the scenes.
+        </span>
+      </div>
+
+      {/* Last section */}
+
+      <div></div>
+
+      {/* Footer */}
+
+      <div className="max-w-[1168px] mx-auto pt-20">
+        <div className="mx-auto w-full h-[3px] bg-black" />
+        <div className="flex">
+          <div className="w-[60%]">
+            <ul className="text-black pt-10 text-[18px]">
+              <li>Sven Fydrich</li>
+              <li>Gloria Bichler</li>
+              <li>Florian von Basse</li>
+              <li>Emanuel Ostertag</li>
+              <li>Katharina Brandtner</li>
+            </ul>
+          </div>
+          <div className="flex flex-col pl-30 pt-10">
+            <span className="text-black w-[70%] text-[18px]">
+              Wir bedanken uns blabla... Lorem ipsum dolor sit amet consectetur.
+              Odio adipiscing vitae risus quam nascetur orci et. Magna integer
+              in sem id netus. Nisl non tristique cursus eget congue. Tristique
+              sagittis nisi magna nulla in tortor. Sem ornare eu interdum nulla.{" "}
+            </span>
+            <span className="text-black w-[70%] pt-10 text-[18px]">
+              Vestibulum et tempus accumsan consectetur ullamcorper sed ac
+              dignissim. Libero posuere et amet sit velit orci quis eu. Et
+              rhoncus consectetur tempus viverra. Sodales duis venenatis feugiat
+              sit arcu elit cons.
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
